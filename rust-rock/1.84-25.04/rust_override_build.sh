51679706c927bc2d9591d6ca097329068f481fe4
marcin <marcin.konowalczyk@canonical.com>
2025-09-16 16:34:44 +0100

    feat: rust sdk rock definition

---
+++ rust-rock/1.84-25.04/rust_override_build.sh
@@ -0,0 +1,48 @@
+set -e
+# spellchecker: ignore rustc binutils libgcc
+
+function main() {
+    local release="./"
+    local slices=(
+        cargo-1.84_cargo
+        rustc-1.84_rustc
+        gcc-14-"$CRAFT_ARCH_TRIPLET_BUILD_FOR"_gcc-14
+        binutils-"$CRAFT_ARCH_TRIPLET_BUILD_FOR"_linker
+        libgcc-14-dev_libgcc
+    )
+    # we need cpp and as for gcc to be able to create executables
+    slices+=(
+        cpp-14-"$CRAFT_ARCH_TRIPLET_BUILD_FOR"_cc1
+        binutils-"$CRAFT_ARCH_TRIPLET_BUILD_FOR"_assembler
+    )
+    # cargo needs ca-certificates to be able to download crates.io index
+    slices+=(
+        ca-certificates_data
+    )
+
+    chisel cut --release "$release" \
+        --root "$CRAFT_PART_INSTALL" \
+        "${slices[@]}"
+
+    ln -s cargo-1.84 "$CRAFT_PART_INSTALL"/usr/bin/cargo
+    ln -s rustc-1.84 "$CRAFT_PART_INSTALL"/usr/bin/rustc
+    ln -s \
+        "$CRAFT_ARCH_TRIPLET_BUILD_FOR"-gcc-14 \
+        "$CRAFT_PART_INSTALL"/usr/bin/cc
+    ln -s \
+        "$CRAFT_ARCH_TRIPLET_BUILD_FOR"-ld \
+        "$CRAFT_PART_INSTALL"/usr/bin/ld
+    ln -s \
+        "$CRAFT_ARCH_TRIPLET_BUILD_FOR"-as \
+        "$CRAFT_PART_INSTALL"/usr/bin/as
+    # ln -s \
+    #     "$CRAFT_ARCH_TRIPLET_BUILD_FOR"-ar \
+    #     "$CRAFT_PART_INSTALL"/usr/bin/ar
+
+    # Also link gcc-14 to gcc, so that build scripts that expect to find `gcc` in PATH work correctly.
+    ln -s \
+        "$CRAFT_ARCH_TRIPLET_BUILD_FOR"-gcc-14 \
+        "$CRAFT_PART_INSTALL"/usr/bin/gcc
+}
+
+main "$@"
