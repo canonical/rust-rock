58834e2029a44394b54a524aacfaabb44d37c99a
marcin <marcin.konowalczyk@canonical.com>
2025-09-16 16:35:13 +0100

    feat(testing): test and version

--- rust-rock/1.84-25.04/spread/general/test/test_help_and_version.sh
+++ rust-rock/1.84-25.04/spread/general/test/test_help_and_version.sh
@@ -8,5 +8,17 @@ fi
 ## TESTS 
 # spellchecker: ignore rustc
 
-# docker run --rm -v "$PWD":/workdir rust-rock:latest \
-#     exec rustc --version
+# cargo
+docker run --rm rust-rock:latest exec cargo --help | grep -q "Rust's package manager"
+docker run --rm rust-rock:latest exec cargo --version | grep -q 'cargo 1.84'
+
+# rust
+docker run --rm rust-rock:latest exec rustc --help | grep -q "Usage: rustc"
+docker run --rm rust-rock:latest exec rustc --version | grep -q 'rustc 1.84'
+
+# gcc
+docker run --rm rust-rock:latest exec gcc --help | grep -q "Usage: gcc"
+gcc_version=$(docker run --rm rust-rock:latest exec gcc --version | head -n1)
+echo "$gcc_version"
+echo "$gcc_version" | grep -q "gcc"
+echo "$gcc_version" | grep -q "14."
