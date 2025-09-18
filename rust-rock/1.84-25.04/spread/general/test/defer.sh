adb37e61300402b89c7a1c334deee158afe3b8e4
marcin <marcin.konowalczyk@canonical.com>
2025-09-18 13:33:27 +0100

    feat: defer self-test

--- rust-rock/1.84-25.04/spread/general/test/defer.sh
+++ rust-rock/1.84-25.04/spread/general/test/defer.sh
@@ -15,6 +15,69 @@ if [[ -z "${__DEFER_SH__:-}" ]]; then
             trap -- "$new_cmd" "$defer_name" || _fatal "unable to modify trap ${defer_name}"
         done
     }
+    declare -f -t trap_add
+
+    ############################################################################
+    # Self-test when run directly with --test
+    if [[ "${BASH_SOURCE[0]}" == "$0" && "$1" == "--test" ]]; then
+        function test_basic() {
+            test_var=0
+            defer "test_var=1" USR1
+            test "$test_var" -eq 0 || return 1
+            kill -USR1 $$
+            test "$test_var" -eq 1 || return 1
+        }
+
+        function test_defer_order() {
+            output=""
+            defer "output+='1'" USR1
+            defer "output+='2'" USR1
+            defer "output+='3'" USR1
+            kill -USR1 $$
+            test "$output" = "321" || return 1
+        }
+
+        function test_tolerates_trailing_semicolon() {
+            test_var=0
+            defer "test_var=1;" USR1
+            test "$test_var" -eq 0 || return 1
+            kill -USR1 $$
+            test "$test_var" -eq 1 || return 1
+        }
+
+        function test_captures_status() {
+            # When using $? we see the status of the previous deferred command
+            test "$(
+                defer 'echo $?' EXIT
+                defer 'false' EXIT
+                exit 99
+            )" -eq 1 || return 1
+            # But $status captures the status of the command that triggered the trap
+            test "$(
+                defer 'echo $status' EXIT
+                defer 'false' EXIT
+                exit 99
+            )" -eq 99 || return 1
+        }
+
+        status=0
+        # Find all the test functions and run them
+        for test_func in $(declare -F | awk '{print $3}' | grep '^test_' | sort); do
+            printf "Running %s... " "$test_func"
+            if $test_func; then
+                printf "\e[32mpass\e[0m\n"
+            else
+                printf "\e[31mfail\e[0m\n"
+                status=1
+            fi
+        done
+
+        if [[ $status -eq 0 ]]; then
+            echo -e "\e[32mSelf-test passed\e[0m"
+        else
+            echo -e "\e[31mSelf-test failed\e[0m"
+        fi
+    fi
 
     export __DEFER_SH__=1
 fi
