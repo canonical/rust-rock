5e21977d441e460ba1f9b48070a23a5ea262b93b
marcin <marcin.konowalczyk@canonical.com>
2025-09-18 17:52:30 +0100

    fix: add --bootstrap to cargo cargo test

--- rust-rock/1.84-25.04/spread/general/test/test_cargo_cargo.sh
+++ rust-rock/1.84-25.04/spread/general/test/test_cargo_cargo.sh
@@ -26,6 +26,7 @@ docker start "$name" 2>/dev/null || true
 defer "docker rm --force $name &>/dev/null || true;" EXIT
 
 # Install dependencies of cargo
+docker exec "$name" apt-get --bootstrap
 docker exec "$name" apt-get update
 docker exec "$name" apt-get install -y libssl-dev pkg-config
 
