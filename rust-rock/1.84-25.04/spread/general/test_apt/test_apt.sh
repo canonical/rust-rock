4f8554dc5dd1848160d33fa5077c9e960e55ef99
marcin <marcin.konowalczyk@canonical.com>
2025-10-02 14:37:51 +0100

    feat: use launch_container in apt test

--- rust-rock/1.84-25.04/spread/general/test_apt/test_apt.sh
+++ rust-rock/1.84-25.04/spread/general/test_apt/test_apt.sh
@@ -2,17 +2,8 @@
 
 # shellcheck source=../../lib/common.sh
 source common.sh
-# shellcheck source=../../lib/defer.sh
-source defer.sh
 
-## TESTS 
-# spellchecker: ignore doctests rustdoc libpam tzdata coreutils
-
-name=test_apt
-docker rm -f "$name" 2>/dev/null || true
-docker create --name "$name" rust-rock:latest > /dev/null
-docker start "$name" 2>/dev/null || true
-defer "docker rm --force $name &>/dev/null || true;" EXIT
+name=$(launch_container apt)
 
 # Run apt update
 docker exec "$name" apt update
